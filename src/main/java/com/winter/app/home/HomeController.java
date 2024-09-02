5722c9b0e70729e284d44e8ed6041c7602816338
Daeki <kimdaeki@gmail.com>
2024-09-02 17:05:31 +0900

    done

--- src/main/java/com/winter/app/home/HomeController.java
+++ src/main/java/com/winter/app/home/HomeController.java
@@ -1,16 +1,22 @@
 package com.winter.app.home;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.winter.app.notice.NoticeMapper;
 
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private NoticeMapper noticeDAO;
 
 	@GetMapping("/")
 	public String home()throws Exception{
+		noticeDAO.getList();
 		return "index";
 	}
 
