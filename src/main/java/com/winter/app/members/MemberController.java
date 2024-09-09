c96be9a707ecf24198ac3f711900d9d9378d6f02
Daeki <kimdaeki@gmail.com>
2024-09-09 16:12:03 +0900

    s2

--- src/main/java/com/winter/app/members/MemberController.java
+++ src/main/java/com/winter/app/members/MemberController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.winter.app.validate.MemberAddGroup;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -19,6 +21,27 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	@GetMapping("update")
+	public String update(HttpSession session, Model model)throws Exception{
+		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+		model.addAttribute("memberVO", memberVO);
+		return "member/update";
+	}
+	
+	@PostMapping("update")
+	public String update(@Validated(MemberAddGroup.class) MemberVO memberVO, BindingResult bindingResult) throws Exception{
+		if(bindingResult.hasErrors()) {
+			return "member/update";
+		}
+		
+		return "redirect:./mypage";
+	}
+	
+	@GetMapping("mypage")
+	public void mypage()throws Exception{
+		
+	}
+	
 	//add
 	@GetMapping("add")
 	public void add(MemberVO memberVO)throws Exception{
@@ -26,7 +49,7 @@ public class MemberController {
 	}
 	
 	@PostMapping("add")
-	public String add(@Valid MemberVO memberVO, BindingResult bindingResult)throws Exception{
+	public String add(@Validated(MemberAddGroup.class) MemberVO memberVO, BindingResult bindingResult)throws Exception{
 		boolean check = memberService.memberValidate(memberVO, bindingResult);
 		if(check) {
 			return "member/add";
--- src/main/java/com/winter/app/members/MemberVO.java
+++ src/main/java/com/winter/app/members/MemberVO.java
@@ -3,6 +3,9 @@ package com.winter.app.members;
 import java.sql.Date;
 import java.util.List;
 
+import com.winter.app.validate.MemberAddGroup;
+import com.winter.app.validate.MemberUpdateGroup;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
@@ -13,19 +16,21 @@ import lombok.Data;
 @Data
 public class MemberVO {
 	
-	@NotBlank
+	@NotBlank(groups = {MemberAddGroup.class, MemberUpdateGroup.class})
 	private String username;
 	
-	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*\\\\\\\\W)(?=\\\\\\\\S+$).{6,12}")
+	
+	@Pattern(groups = {MemberAddGroup.class}, regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*\\\\\\\\W)(?=\\\\\\\\S+$).{6,12}")
+	@NotBlank(groups = {MemberAddGroup.class})
 	private String password;
 	
 	private String passwordCheck;
 	
-	@NotBlank
+	@NotBlank(groups = {MemberAddGroup.class, MemberUpdateGroup.class})
 	private String name;
-	@Email
+	@Email(groups = {MemberAddGroup.class, MemberUpdateGroup.class})
 	private String email;
-	@Past
+	@Past(groups = {MemberAddGroup.class, MemberUpdateGroup.class})
 	private Date birth;
 	private boolean enabled;
 	private List<RoleVO> vos;
